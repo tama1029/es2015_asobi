9ab9cc717534832e55f871224e32952b27034f9d
Takanori Tamaki <takanori.tamaki@speee.jp>
2016-12-20 14:57:56 +0900

    データモデル作る場合の確認

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // class
 class Human {
   constructor(name) {
-    this.name = name;
+    this._name = name;
   }
+  // prototypeメソッド　インスタンスから呼び出せるメソッド
   hello() {
-    console.log('My name is ' + this.name);
+    console.log('My name is ' + this._name);
   }
   // ruby でいうところのself.かな インスタンスの生成無しに呼び出せるメソッド
   static num_of_hands() {
@@ -18,20 +19,69 @@ hello.hello();
 hello2 = new Human('tama2');
 hello2.hello();
 
+//----------------------------------------------------------------//
+
+// Data model
+class Neko {
+    constructor(name, type) {
+        this._name = name;
+        this._type = type;
+    }
+
+    // constructorは１個のみ
+    // constructor(name) {
+    //     this._name = name;
+    // }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(val){
+        if (val) {
+            this._name= val;
+        } else {
+            this._name = 'No Name';
+        }
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    set type(val){
+        if (val) {
+            this._type= val;
+        } else {
+            this._type = 'No Name';
+        }
+    }
+}
+
+neko = new Neko('tama', 'syamu');
+console.log(neko.name);
+console.log(neko.type);
+neko.name = 'tama2';
+neko.type = 'syamu2';
+console.log(neko.name);
+console.log(neko.type);
+
+//----------------------------------------------------------------//
+
 // クラスの継承 constructor内にsuper書かなくても自動で親側がつかわれるみたい
 class Animal {
     constructor(name) {
-        this.name = name;
+        this._name = name;
     }
 
     speak() {
-        console.log(this.name + ' makes a noise.');
+        console.log(this._name + ' makes a noise.');
     }
 }
 
 class Dog extends Animal {
     speak() {
-        console.log(this.name + ' barks.');
+        console.log(this._name + ' barks.');
     }
 }
 
