10bf86946a71e1526609d428599243a7e9abd1b4
Takanori Tamaki <takanori.tamaki@speee.jp>
2016-12-20 10:57:05 +0900

    遊びことはじめ

---
+++ main.js
@@ -0,0 +1,13 @@
+class Human {
+  constructor(name) {
+    this.name = name;
+  }
+  hello() {
+    console.log('My name is ' + this.name);
+  }
+  static num_of_hands() {
+    console.log(2)
+  }
+}
+
+Human.num_of_hands()
