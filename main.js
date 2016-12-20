54d57e5329773ece6821a5bf734d75f3e05e32e9
Takanori Tamaki <takanori.tamaki@speee.jp>
2016-12-20 11:12:03 +0900

    継承について記述

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ hello = new Human('tama');
 hello.hello();
 hello2 = new Human('tama2');
 hello2.hello();
+
+// クラスの継承 constructor内にsuper書かなくても自動で親側がつかわれるみたい
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(this.name + ' makes a noise.');
+    }
+}
+
+class Dog extends Animal {
+    speak() {
+        console.log(this.name + ' barks.');
+    }
+}
+
+dog = new Dog('poti');
+dog.speak();
